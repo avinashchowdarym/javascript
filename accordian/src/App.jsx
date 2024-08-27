a4c501817b2cdad57cdce415d3176d70cf280119
avinashchowdarym <151068758+avinashchowdarym@users.noreply.github.com>
2024-08-27 13:39:24 +0000

    accordian completed styles are yet to be added. logic is completed

--- accordian/src/App.jsx
+++ accordian/src/App.jsx
@@ -3,24 +3,24 @@ import './App.css'
 
 const data = [
   {
-    title: 'this is Title 1',
+    title: 'Title 1',
     content: ' This is a paragraph .. This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph '
     
   },
   {
-    title: 'this is Title 1',
+    title: 'Title 2',
     content: ' This is a paragraph .. This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph '
     
   },{
-    title: 'this is Title 1',
+    title: 'Title 3',
     content: ' This is a paragraph .. This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph '
     
   },{
-    title: 'this is Title 1',
+    title: 'Title 4',
     content: ' This is a paragraph .. This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph '
     
   },{
-    title: 'this is Title 1',
+    title: 'Title 5',
     content: ' This is a paragraph .. This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph ..This is a paragraph '
     
   },
@@ -32,23 +32,24 @@ function App() {
   const [selector,setSelector] = useState();
   const handleClick = (index) =>{
     if(selector === index){
-      setSelector();
+      setSelector(null);
     }
     setSelector(index);
   }
-
+  
 
   return (
    <div className='App'>
     {
       data.map((item,index)=>{
         return(
-          <div key={index} onClick={()=>{handleClick(index)}}>
+          <div key={index} onClick={()=>{handleClick(index)}} className='box'>
             <div className='title' >{item.title}</div>
-            <div className='content hidden'>{item.content}</div>
+            <div className= {selector === index ? 'content-active' : 'content'}>{item.content}</div>
             </div>
         )
-      })
+      }
+    )
   }
     
    </div>
