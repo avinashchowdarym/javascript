340281cd37558ae6922196f79ab39c88be4ca704
avinashchowdarym <151068758+avinashchowdarym@users.noreply.github.com>
2025-02-04 21:19:33 +0530

    updated memoize function

--- memoize.js
+++ memoize.js
@@ -2,16 +2,19 @@ function sum(a,b,c){
     return a+b+c;
 }
 
-function memoize(fn){
-    var result = {};
-    return function(...args){
-        const key = JSON.stringify(args);
-       if(result[key]) return result[key];
-       else{
-        result[key] = fn(...args);
-        return result[key];
-       }
+function memoize(fn) {
+    // write your solution here
+    let cache = {};
+    return (...args)=> {
+      const key = JSON.stringify(args);
+      if(key in cache){
+        return cache[key];
+      }
+      else{
+        const result = fn.apply(this, args);
+        cache[key] = result;
+        return result;
+      }
     }
-
 }
 console.log(memoize(sum)(1,2,3));
